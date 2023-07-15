0f42f2c49cbac5eae73a5d441de6e27e5fe7540b
ahakDot <129846041+ahakDot@users.noreply.github.com>
2023-07-15 22:13:15 +0200

    PinsExpander Use

--- Software/Smart_Schloss/src/main.cpp
+++ Software/Smart_Schloss/src/main.cpp
@@ -2,10 +2,12 @@
 #include <string>
 #include <WiFi.h>
 
+//Classes written by me
 #include "Lock.h"
 #include "DoorState.h"
 #include "PN532.h"
 #include "ManagingWifi.h"
+#include "PinsExpander.h"
 
 //Pins-Expander section 
 //Use this variables to turn the wished MCP23017 pin(s) on/off.
@@ -19,14 +21,17 @@
   #define A5 5
   #define A6 6
   #define A7 7
-  #define B1 8
-  #define B2 9
-  #define B3 10
-  #define B4 11
-  #define B5 12
-  #define B6 13
-  #define B7 14
-  
+  #define B0 8
+  #define B1 9
+  #define B2 10
+  #define B3 11
+  #define B4 12
+  #define B5 13
+  #define B6 14
+  #define B7 15
+  PinsExpander pinsExpander(0x20,SDA_MCP,SCL_MCP);
+  const int outputPins[] = {B1,B3,A5};
+  int numberOfOutputPins = sizeof(outputPins) / sizeof(outputPins[0]);
 //........................ -> // Uncomment the wished line when using the MCP23017
 
 //RFID section
@@ -43,21 +48,21 @@ const char* password = "123456789"; //TODO: Change wifi data to the wished one
 
 
 int lockState=-1;
-PN532 pn532(PN532_SDA, PN532_SCL);
-Lock lock1;
-DoorState doorstate;
+//PN532 pn532(PN532_SDA, PN532_SCL);
+//Lock lock1;
+//DoorState doorstate;
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
   //Setup PN532-----------------------------------------------------------------------------------
-  pn532.begin();
+  /*pn532.begin();
   uint32_t versiondata = pn532.nfc.getFirmwareVersion();
   if (!versiondata) {
     Serial.println("Didn't find PN53x board");
     while (1);
   }
-  pn532.nfc.SAMConfig();
-  //End PN532 setup-------------------------------------------------------------------------------
+  pn532.nfc.SAMConfig();*/
+  //End PN532 setup-------------------------------------------------------------------------------//
 
   //WiFi------------------------------------------------------------------------------------------
   WiFi.begin(ssid,password);
@@ -70,15 +75,21 @@ void setup() {
   Serial.println(WiFi.localIP());
   WiFi.setAutoConnect(true);
   WiFi.setAutoReconnect(true);
-  //End WiFi--------------------------------------------------------------------------------------
+  //End WiFi--------------------------------------------------------------------------------------//
 
-  lock1.setup();
-  doorstate.setup();
+  //Pins Expander---------------------------------------------------------------------------------
+  pinsExpander.setup();
+  Wire.begin(SDA_MCP,SCL_MCP);
+  pinsExpander.setPinModeOutput(outputPins,numberOfOutputPins);
+  //End Pins Expander-----------------------------------------------------------------------------//
+  
+  //lock1.setup();
+  //doorstate.setup();
 }
 
 void loop() {
   //Reads the RFID card here-----------------------------------
-    if (pn532.isCardPresent()) {
+    /*if (pn532.isCardPresent()) {
     Serial.println("Found an RFID card or tag!");
     uint8_t uid[7];
     uint8_t uidLength;
@@ -93,12 +104,19 @@ void loop() {
     }
     Serial.println();
     delay(2000);
-  }
-  //---------------------------------------------------------------
+  }*/
+  //---------------------------------------------------------------//
 
-  doorstate.checkState();
+  //Open lock or turn LED on using PE----------------------------------------
+  pinsExpander.TurnHigh(outputPins,numberOfOutputPins);
+  delay(1000);
+  pinsExpander.TurnLow(outputPins,numberOfOutputPins);
+  delay(1000);
+  //--------------------------------------------------------------//
+  
+  /*doorstate.checkState();
   lockState=lock1.OpenLock("content");
   if(lockState==1){
     content="";
-    }
+    }*/
 }
